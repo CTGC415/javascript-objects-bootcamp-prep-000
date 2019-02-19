35218a1eeb6e0a2bd63683b52b3c92bb11b9c113
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-19 16:40:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {ACDC: "TNT", 'Super Tramp': "School"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
